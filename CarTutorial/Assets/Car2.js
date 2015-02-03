a61b30ae90005d548dade0962fb36ce5933faea6
vsawyer <vsawyer1@ualberta.ca>
2015-02-03 14:34:37 -0700

    two simple cars controlled by wasd and rrow keys

---
+++ CarTutorial/Assets/Car2.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var forwardSpeed: float = 5;
+var turnSpeed: float = 2;
+function Start () {
+
+}
+
+function Update () {
+	var forwardMoveAmount= Input.GetAxis("ws")*forwardSpeed;
+	var turnAmount = Input.GetAxis("ad")*turnSpeed;
+	
+	transform.Rotate(0,turnAmount,0);
+	rigidbody.AddRelativeForce(0,0,forwardMoveAmount);
+}
